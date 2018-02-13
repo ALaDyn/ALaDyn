6bf0ccc24290be02d50e2695698a847b4db36e34
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-02-14 00:05:07 +0100

    rewrite FindFFTW.cmake in a more modern way

--- cmake/Modules/FindFFTW.cmake
+++ cmake/Modules/FindFFTW.cmake
@@ -1,5 +1,26 @@
-# - Find the FFTW library
+# Distributed under the OSI-approved BSD 3-Clause License.
+# Copyright Stefano Sinigardi
 #
+#.rst:
+# FindFFTW
+# --------
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project::
+#
+#  FFTW_FOUND          - True if FFTW found on the local system
+#  FFTW_INCLUDE_DIRS   - Location of FFTW header files.
+#  FFTW_LIBRARIES      - The FFTW libraries.
+#
+# Hints
+# ^^^^^
+#
+# Set ``FFTW_ROOT_DIR`` to a directory that contains a FFTW installation.
+#
+# This script expects to find libraries at ``$FFTW_ROOT_DIR/lib`` and the
+# headers at ``$FFTW_ROOT_DIR/include/fftw3``.
 # Usage:
 #   find_package(FFTW [REQUIRED] [QUIET] )
 #
@@ -16,104 +37,182 @@
 #   FFTW_INCLUDE_DIR        ... fftw include directory
 #
 
-#If environment variable FFTWDIR is specified, it has same effect as FFTW_ROOT
-if(NOT FFTW_ROOT AND ENV{FFTWDIR})
-  set(FFTW_ROOT $ENV{FFTWDIR} )
-endif()
-
-# Check if we can use PkgConfig
-find_package(PkgConfig)
-
-#Determine from PKG
-if( PKG_CONFIG_FOUND AND NOT FFTW_ROOT )
-  pkg_check_modules( PKG_FFTW QUIET "fftw3" )
-endif()
-
-#Check whether to search static or dynamic libs
-set( CMAKE_FIND_LIBRARY_SUFFIXES_SAV ${CMAKE_FIND_LIBRARY_SUFFIXES} )
-
-if( ${FFTW_USE_STATIC_LIBS} )
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} )
-else()
-  set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
-endif()
-
-if( FFTW_ROOT )
-
-  #find libs
-  find_library(
-    FFTW_LIB
-    NAMES "fftw3"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  find_library(
-    FFTWL_LIB
-    NAMES "fftw3l"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  #find includes
-  find_path(
-    FFTW_INCLUDES
-    NAMES "fftw3.h"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "include"
-    NO_DEFAULT_PATH
-  )
-
-else()
-
-  find_library(
-    FFTW_LIB
-    NAMES "fftw3"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-
-
-  find_library(
-    FFTWL_LIB
-    NAMES "fftw3l"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-
-  find_path(
-    FFTW_INCLUDES
-    NAMES "fftw3.h"
-    PATHS ${PKG_FFTW_INCLUDE_DIRS} ${INCLUDE_INSTALL_DIR}
-  )
-
-endif( FFTW_ROOT )
-
-set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTWF_LIB})
-
-if(FFTWL_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
-endif()
-
-set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_SAV} )
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FFTW DEFAULT_MSG
-                                  FFTW_INCLUDES FFTW_LIBRARIES)
 
-mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES FFTW_LIB FFTWF_LIB FFTWL_LIB)
+#=============================================================================
+# If the user has provided ``FFTW_ROOT_DIR``, use it!  Choose items found
+# at this location over system locations.
+if( EXISTS "$ENV{FFTW_ROOT_DIR}" )
+  file( TO_CMAKE_PATH "$ENV{FFTW_ROOT_DIR}" FFTW_ROOT_DIR )
+  set( FFTW_ROOT_DIR "${FFTW_ROOT_DIR}" CACHE PATH "Prefix for FFTW installation." )
+endif()
+if( NOT EXISTS "${FFTW_ROOT_DIR}" )
+  set( FFTW_USE_PKGCONFIG ON )
+endif()
 
+
+#=============================================================================
+# As a first try, use the PkgConfig module.  This will work on many
+# *NIX systems.  See :module:`findpkgconfig`
+# This will return ``FFTW_INCLUDEDIR`` and ``FFTW_LIBDIR`` used below.
+if( FFTW_USE_PKGCONFIG )
+  find_package(PkgConfig)
+  pkg_check_modules( FFTW QUIET fftw3 )
+
+  if( EXISTS "${FFTW_INCLUDEDIR}" )
+    get_filename_component( FFTW_ROOT_DIR "${FFTW_INCLUDEDIR}" DIRECTORY CACHE)
+  endif()
+endif()
+
+#=============================================================================
+# Set FFTW_INCLUDE_DIRS and FFTW_LIBRARIES. If we skipped the PkgConfig step, try
+# to find the libraries at $FFTW_ROOT_DIR (if provided) or in standard system
+# locations.  These find_library and find_path calls will prefer custom
+# locations over standard locations (HINTS).  If the requested file is not found
+# at the HINTS location, standard system locations will be still be searched
+# (/usr/lib64 (Redhat), lib/i386-linux-gnu (Debian)).
+
+find_path( FFTW_INCLUDE_DIR
+  NAMES fftw3.h
+  HINTS ${FFTW_ROOT_DIR}/include ${FFTW_INCLUDEDIR}
+)
+find_library( FFTW_LIBRARY
+  NAMES fftw3
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Release Debug
+)
+
+find_library( FFTWF_LIBRARY
+  NAMES fftw3f
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Release Debug
+)
+
+find_library( FFTWL_LIBRARY
+  NAMES fftw3l
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Release Debug
+)
+
+# Do we also have debug versions?
+find_library( FFTW_LIBRARY_DEBUG
+  NAMES fftw3d
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Debug
+)
+
+find_library( FFTWF_LIBRARY_DEBUG
+  NAMES fftw3fd
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Debug
+)
+
+find_library( FFTWL_LIBRARY_DEBUG
+  NAMES fftw3ld
+  HINTS ${FFTW_ROOT_DIR}/lib ${FFTW_LIBDIR}
+  PATH_SUFFIXES Debug
+)
+
+set( FFTW_INCLUDE_DIRS ${FFTW_INCLUDE_DIR} )
+set( FFTW_LIBRARIES ${FFTW_LIBRARY} ${FFTWF_LIBRARY} ${FFTWL_LIBRARY} )
+
+#=============================================================================
+# handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if all
+# listed variables are TRUE
+find_package_handle_standard_args( FFTW
+  FOUND_VAR
+    FFTW_FOUND
+  REQUIRED_VARS
+    FFTW_INCLUDE_DIR
+    FFTW_LIBRARY
+    FFTWF_LIBRARY
+    FFTWF_LIBRARY
+  )
+
+mark_as_advanced( FFTW_ROOT_DIR FFTW_LIBRARY FFTW_INCLUDE_DIR
+  FFTWF_LIBRARY FFTWL_LIBRARY FFTW_LIBRARY_DEBUG FFTWF_LIBRARY_DEBUG FFTWL_LIBRARY_DEBUG
+  FFTW_USE_PKGCONFIG FFTW_CONFIG )
+
+
+#=============================================================================
+# Register imported libraries:
+# 1. If we can find a Windows .dll file (or if we can find both Debug and
+#    Release libraries), we will set appropriate target properties for these.
+# 2. However, for most systems, we will only register the import location and
+#    include directory.
+
+# Look for dlls, or Release and Debug libraries.
+if(WIN32)
+  string( REPLACE ".lib" ".dll" FFTW_LIBRARY_DLL        "${FFTW_LIBRARY}" )
+  string( REPLACE ".lib" ".dll" FFTWF_LIBRARY_DLL       "${FFTWF_LIBRARY}" )
+  string( REPLACE ".lib" ".dll" FFTWL_LIBRARY_DLL       "${FFTWL_LIBRARY}" )
+  string( REPLACE ".lib" ".dll" FFTW_LIBRARY_DEBUG_DLL  "${FFTW_LIBRARY_DEBUG}" )
+  string( REPLACE ".lib" ".dll" FFTWF_LIBRARY_DEBUG_DLL "${FFTWF_LIBRARY_DEBUG}" )
+  string( REPLACE ".lib" ".dll" FFTWL_LIBRARY_DEBUG_DLL "${FFTWL_LIBRARY_DEBUG}" )
+endif()
+
+if( FFTW_FOUND AND NOT TARGET FFTW::fftw3 )
+  if( EXISTS "${FFTW_LIBRARY_DLL}" AND EXISTS "${FFTWF_LIBRARY_DLL}" AND EXISTS "${FFTWL_LIBRARY_DLL}" )
+
+    # Windows systems with dll libraries.
+    add_library( FFTW::fftw3      SHARED IMPORTED )
+    add_library( FFTW::fftw3l     SHARED IMPORTED )
+    add_library( FFTW::fftw3f     SHARED IMPORTED )
+
+    # Windows with dlls, but only Release libraries.
+    set_target_properties( FFTW::fftw3f PROPERTIES
+      IMPORTED_LOCATION_RELEASE         "${FFTWF_LIBRARY_DLL}"
+      IMPORTED_IMPLIB                   "${FFTWF_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_CONFIGURATIONS           Release
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+    set_target_properties( FFTW::fftw3l PROPERTIES
+      IMPORTED_LOCATION_RELEASE         "${FFTWL_LIBRARY_DLL}"
+      IMPORTED_IMPLIB                   "${FFTWL_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_CONFIGURATIONS           Release
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+    set_target_properties( FFTW::fftw3 PROPERTIES
+      IMPORTED_LOCATION_RELEASE         "${FFTW_LIBRARY_DLL}"
+      IMPORTED_IMPLIB                   "${FFTW_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_CONFIGURATIONS           Release
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+
+    # If we have both Debug and Release libraries
+    if( EXISTS "${FFTW_LIBRARY_DEBUG_DLL}" AND EXISTS "${FFTWF_LIBRARY_DEBUG_DLL}" AND EXISTS "${FFTWL_LIBRARY_DEBUG_DLL}" )
+      set_property( TARGET FFTW::fftw3f APPEND PROPERTY IMPORTED_CONFIGURATIONS Debug )
+      set_target_properties( FFTW::fftw3f PROPERTIES
+        IMPORTED_LOCATION_DEBUG           "${FFTWF_LIBRARY_DEBUG_DLL}"
+        IMPORTED_IMPLIB_DEBUG             "${FFTWF_LIBRARY_DEBUG}" )
+      set_property( TARGET FFTW::fftw3l APPEND PROPERTY IMPORTED_CONFIGURATIONS Debug )
+      set_target_properties( FFTW::fftw3l PROPERTIES
+        IMPORTED_LOCATION_DEBUG           "${FFTWL_LIBRARY_DEBUG_DLL}"
+        IMPORTED_IMPLIB_DEBUG             "${FFTWL_LIBRARY_DEBUG}" )
+      set_property( TARGET FFTW::fftw3 APPEND PROPERTY IMPORTED_CONFIGURATIONS Debug )
+      set_target_properties( FFTW::fftw3 PROPERTIES
+        IMPORTED_LOCATION_DEBUG           "${FFTW_LIBRARY_DEBUG_DLL}"
+        IMPORTED_IMPLIB_DEBUG             "${FFTW_LIBRARY_DEBUG}" )
+    endif()
+
+  else()
+
+    # For all other environments (ones without dll libraries), create
+    # the imported library targets.
+    add_library( FFTW::fftw3      UNKNOWN IMPORTED )
+    add_library( FFTW::fftw3f     UNKNOWN IMPORTED )
+    add_library( FFTW::fftw3l     UNKNOWN IMPORTED )
+    set_target_properties( FFTW::fftw3f PROPERTIES
+      IMPORTED_LOCATION                 "${FFTWF_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+    set_target_properties( FFTW::fftw3l PROPERTIES
+      IMPORTED_LOCATION                 "${FFTWL_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+    set_target_properties( FFTW::fftw3 PROPERTIES
+      IMPORTED_LOCATION                 "${FFTW_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES     "${FFTW_INCLUDE_DIRS}"
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
+  endif()
+endif()
